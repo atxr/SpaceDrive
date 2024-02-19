fa7045bf1c474288d5d2508ec6996cc2802c1b8f
atxr <alexandre.tullot@gmail.com>
2024-02-19 14:54:37 +0100

    Add bitstream utility

---
+++ libmineziper/include/libmineziper_bitstream.h
@@ -0,0 +1,20 @@
+#ifndef LIBMINEZIPER_BITSTREAM_H
+#define LIBMINEZIPER_BITSTREAM_H
+
+typedef struct bitstream
+{
+  char *data;
+  int data_size;        // size of 'data' array
+  int last_bit_offset;  // last bit in the stream
+
+  int current_data_offset;  // position in 'data', i.e.
+                            // data[current_data_offset] is current
+                            // reading/writing byte
+  int current_bit_offset;   // which bit we are currently reading/writing
+} bitstream;
+
+char get_bits(bitstream *bs, unsigned int size);
+void print_bits(int x, int size);
+unsigned int reverse(unsigned int x, unsigned int numBits);
+
+#endif
