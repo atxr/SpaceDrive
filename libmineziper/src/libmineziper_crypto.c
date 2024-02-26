43ac716bf2eaf1b48f5b197f069d1e8f3ac313a0
atxr <alexandre.tullot@gmail.com>
2024-02-26 10:20:35 +0100

    Add crc32 function

---
+++ libmineziper/src/libmineziper_crypto.c
@@ -0,0 +1,12 @@
+#include "libmineziper_crypto.h"
+
+unsigned int xcrc32(const unsigned char *buf, int len, unsigned int init)
+{
+  unsigned int crc = init;
+  while (len--)
+  {
+    crc = (crc << 8) ^ crc32_table[((crc >> 24) ^ *buf) & 255];
+    buf++;
+  }
+  return crc;
+}
